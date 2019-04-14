1672f4c6805b5c7382d5d1bf445871b6fc69a316
Sam Amara <amaramas@my.erau.edu>
2019-04-13 22:41:08 -0400

    implemented RouletteGame class and console display in Program

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -10,7 +10,29 @@ namespace Roulette
     {
         static void Main(string[] args)
         {
+            var rouletteGame = new RouletteGame();
 
+            DisplayWinningBets(rouletteGame.SpinWheel());
+        }
+
+        static void DisplayWinningBets(RouletteNumber winningBin)
+        {
+            Console.WriteLine($"Winning bets are :");
+            Console.WriteLine($"\n 1.  Bin number {winningBin.ToString()}");
+
+            if (winningBin.Val != Bin.Zero && winningBin.Val != Bin.ZeroZero)
+            {
+                Console.WriteLine($" 2.  {winningBin.EvenOdd} numbers");
+                Console.WriteLine($" 3.  {winningBin.Color} color");
+                Console.WriteLine($" 4.  {winningBin.LowHigh} numbers");
+                Console.WriteLine($" 5.  {winningBin.Dozen} numbers");
+                Console.WriteLine($" 6.  {winningBin.Column} numbers");
+                Console.WriteLine($" 7.  Street: {winningBin.Row}");
+                Console.WriteLine($" 8.  6 Numbers: {String.Join(" or ", winningBin.DoubleRow)}");
+                Console.WriteLine($" 9.  Splits: {String.Join(" or ", winningBin.Splits)}");
+                Console.WriteLine($" 10. Corners: {String.Join(" or ", winningBin.Corners)}");
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ Roulette/RouletteGame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roulette
+{
+    public class RouletteGame
+    {
+        public RouletteNumber[] RouletteNumbers { get; private set; }
+        private Random randomBin;
+
+        public RouletteGame()
+        {
+            var bins = Enum.GetValues(typeof(Bin));
+            RouletteNumbers = new RouletteNumber[bins.Length];
+
+            for(int i = 0; i < RouletteNumbers.Length; i++)
+            {
+                RouletteNumbers[i] = new RouletteNumber((Bin)bins.GetValue(i));
+            }
+
+            randomBin = new Random();
+        }
+
+        public RouletteNumber SpinWheel()
+        {
+            return RouletteNumbers[randomBin.Next(38)];
+        }
+    }
+}
